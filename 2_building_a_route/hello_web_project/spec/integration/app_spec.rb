b6ad3b4c9986eb5594930cfbac94ecac0788f6cf
Zara <zarabucknor@yahoo.co.uk>
2023-05-31 14:45:29 +0100

    update expected result to = 200 OK

--- 2_building_a_route/hello_web_project/spec/integration/app_spec.rb
+++ 2_building_a_route/hello_web_project/spec/integration/app_spec.rb
@@ -8,8 +8,8 @@ describe Application do
     let(:app) { Application.new }
 
     context "GET /names" do
-        it "should return Julia, Mary, Karim" do
-            response = get("/names?names=Julia_Mary_Karim")
+        it "should return 200 OK" do
+            response = get("/names")
 
             expect(response.status).to eq(200)
             expect(response.body).to eq("Julia, Mary, Karim")
